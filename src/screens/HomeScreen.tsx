7e2a33294ee40cd3363161cf61236ee87cc2305c
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-05-27 19:21:52 +0200

    Adds boiler plate code to screens.

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+    return(
+        <View>
+            <Text>HOME SCREEN</Text>
+        </View>
+    )
+}
