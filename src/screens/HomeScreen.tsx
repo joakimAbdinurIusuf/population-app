e0019e6704892df40e3139b5d2546373eaf326b3
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-05-28 12:44:21 +0200

    Adds function comment to the home screen.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import HomeScreenButton from '../components/HomeScreenButton';
 
+/**
+ * The home screen. The user can navigate to the SearchByCity and SearchByCountry 
+ * screen from here.
+ * 
+ * @returns The home screen.
+ */
 export default function HomeScreen(props: any) {
     return(
         <View style={styles.container}>
@@ -30,19 +36,3 @@ const styles = StyleSheet.create({
         marginVertical: 100,
     },
 });
-
-/*
-return(
-        <View style={styles.container}>
-            <Text style={styles.title}>CityPop</Text> 
-            <Button 
-                title="SEARCH BY CITY" 
-                onPress={() => props.navigation.navigate("SearchByCity")}
-            />
-            <Button 
-                title="SEARCH BY COUNTRY" 
-                onPress={() => props.navigation.navigate("SearchByCountry")}
-            />
-        </View>
-    )
-*/
