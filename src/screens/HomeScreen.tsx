5b4b4a19023b0fcf3434fbebcdadaf2abe8a5dde
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-05-28 11:16:04 +0200

    Adds navigation to SearchByCityScreen and SearchByCountryScreen when corresponding button is pressed.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function HomeScreen() {
+export default function HomeScreen(props: any) {
     return(
         <View style={styles.container}>
             <Text style={styles.title}>CityPop</Text> 
             <Button 
                 title="SEARCH BY CITY" 
-                onPress={() => console.log("pressed")}
+                onPress={() => props.navigation.navigate("SearchByCity")}
             />
             <Button 
                 title="SEARCH BY COUNTRY" 
+                onPress={() => props.navigation.navigate("SearchByCountry")}
             />
         </View>
     )
