a184515454b26792ec3e2a1e35633587907b188b
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-05-29 21:37:07 +0200

    Adds TODOs in the useCity hook.

--- src/hooks/useCity.tsx
+++ src/hooks/useCity.tsx
@@ -5,6 +5,23 @@ export default function useCity() {
     const [result, setResult] = useState([]);
     const [errorMessage, setErrorMessage] = useState("");
 
+    
+    /* 
+    TODOs: !!!!!!!!!!!
+
+    Should maybe include the order by parameter even in this stage. Presumably,
+    if there are two cities called London (which there are), the user probably meant
+    the one with the largest population.
+
+    Should chech that the name key EQUALS (not includes) the search term. 
+    Be careful of whitespace? E.g. will "Stockholm" work but and "Stockholm " fail?
+
+    Might want to remove the cities parameter and have custom logic, e.g. if one searches for 
+    city X, have a for loop that checks each element until it finds one where the name key
+    equals X and the fclName key points to a value that includes the substring city.
+    It should then return the first one it finds, else throw an error message saying the
+    city doesn't exist. 
+    */
     const searchApi: any = async (enteredCity: any) => {
         try {
             const response = await GeoNames.get("/searchJSON", {
