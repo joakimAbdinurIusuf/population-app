246b3e5de7ead9e3ae6a62d71241bc3155a9a0c7
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-06-01 22:30:29 +0200

    Adds cities='cities500' so that only cities are fetched. Before adding this condition to the url a search for Kenya returned the country as the third 'city', but after adding it it only retrieves cities so the request works fine.

--- src/hooks/useCountryCode.tsx
+++ src/hooks/useCountryCode.tsx
@@ -11,47 +11,64 @@ export default function useCountryCode() {
     const [dataIsFetched, setDataIsFetched] = useState(false);
     const [fetchingData, setfetchingData] = useState(false);
 
-    const searchCountryCodeApi: any = async (enteredCountryCode: string) => {
+    const searchCountryCodeApi: any = async (enteredCountry: string) => {
         try {
             setfetchingData(true);
 
-            const url: string = "/searchJSON?orderby='population'&username=weknowit&country=" + enteredCountryCode;
-            const response = await GeoNames.get(url);
-            
-            /* 
-            The following doesn't work for some reason:
-
-            const response = await GeoNames.get("/searchJSON", {
+            const countryCodeResponse = await GeoNames.get("/searchJSON", {
                 params: {
                     username: "weknowit",
-                    name_equals: enteredCountryCode,
-                    orderby: "population",
+                    name_equals: enteredCountry,
                 }
-            }); 
-            
-            */
+            });
 
-            let nameOfBiggestCity = response.data.geonames[0].name;
-            let sizeOfBiggestCity = response.data.geonames[0].population;
-            let nameOfSecondBiggestCity = response.data.geonames[1].name;
-            let sizeOfSecondBiggestCity = response.data.geonames[1].population;
-            let nameOfThirdBiggestCity = response.data.geonames[2].name;
-            let sizeOfThirdBiggestCity = response.data.geonames[2].population;
-            let arr: any = [nameOfBiggestCity, sizeOfBiggestCity,
-                    nameOfSecondBiggestCity, sizeOfSecondBiggestCity,
-                    nameOfThirdBiggestCity, sizeOfThirdBiggestCity];
-
-            console.log(arr);
-
-            if (response.data.geonames.length >= 3) {
-                setThreeBiggestCities(arr);
-                setErrorMessage(""); 
-                setDataIsFetched(true);
-                setfetchingData(false);
-            } else {
+            if (countryCodeResponse.data.geonames.length < 1 || countryCodeResponse.data.geonames[0].name !== enteredCountry) {
                 setErrorMessage("Country doesn't exist, please try again");
                 setDataIsFetched(false);
                 setfetchingData(false);
+            } else {
+                const countryCode = countryCodeResponse.data.geonames[0].countryCode;
+                const url: string = "/searchJSON?orderby='population'&cities='cities500'&username=weknowit&country=" + countryCode;
+                const response = await GeoNames.get(url);
+
+                /* 
+                The following doesn't work for some reason, very strange:
+
+                const response = await GeoNames.get("/searchJSON", {
+                    params: {
+                        username: "weknowit",
+                        name_equals: countryCode,
+                        orderby: "population",
+                        cities: "cities500",
+                    }
+                }); 
+
+                It should be equivalent to the url, but it isn't.
+                
+                */
+
+                let nameOfBiggestCity = response.data.geonames[0].name;
+                let sizeOfBiggestCity = response.data.geonames[0].population;
+                let nameOfSecondBiggestCity = response.data.geonames[1].name;
+                let sizeOfSecondBiggestCity = response.data.geonames[1].population;
+                let nameOfThirdBiggestCity = response.data.geonames[2].name;
+                let sizeOfThirdBiggestCity = response.data.geonames[2].population;
+                let arr: any = [nameOfBiggestCity, sizeOfBiggestCity,
+                        nameOfSecondBiggestCity, sizeOfSecondBiggestCity,
+                        nameOfThirdBiggestCity, sizeOfThirdBiggestCity];
+
+                console.log(arr);
+
+                if (response.data.geonames.length >= 3) {
+                    setThreeBiggestCities(arr);
+                    setErrorMessage(""); 
+                    setDataIsFetched(true);
+                    setfetchingData(false);
+                } else {
+                    setErrorMessage("Country doesn't exist, please try again");
+                    setDataIsFetched(false);
+                    setfetchingData(false);
+                }
             }
              
         } catch (err) {
